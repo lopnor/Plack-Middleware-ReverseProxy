9373efd67d15fc9caca42c31b879722cceac63c6
Nobuo Danjou <nobuo.danjou@gmail.com>
2009-11-29 15:48:32 +0900

    initial commit

---
+++ lib/Plack/Middleware/ReverseProxy.pm
@@ -0,0 +1,33 @@
+package Plack::Middleware::ReverseProxy;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::Middleware::ReverseProxy -
+
+=head1 SYNOPSIS
+
+  use Plack::Middleware::ReverseProxy;
+
+=head1 DESCRIPTION
+
+Plack::Middleware::ReverseProxy is
+
+=head1 AUTHOR
+
+Noubo Danjou E<lt>nobuo.danjou@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
